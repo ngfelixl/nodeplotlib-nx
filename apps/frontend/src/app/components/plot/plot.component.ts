243c5fb6f52f7ca902f8be8ce26a7df382c6e372
Felix Lemke <flemke.dev@gmail.com>
2019-04-10 01:03:20 +0200

    refactor(frontend): refactor page-not-found, refactor custom-theme

--- apps/frontend/src/app/components/plot/plot.component.ts
+++ apps/frontend/src/app/components/plot/plot.component.ts
@@ -1,19 +1,37 @@
-import { Component, ElementRef, AfterViewInit, Input, ChangeDetectionStrategy } from '@angular/core';
-import { newPlot } from 'plotly.js';
+import { Component, ElementRef, AfterViewInit, Input, ChangeDetectionStrategy, HostListener, OnDestroy } from '@angular/core';
+import { newPlot, relayout } from 'plotly.js';
 import { Trace } from '@nodeplotlib/interfaces';
+import { fromEvent, Subject } from 'rxjs';
+import { debounceTime, throttleTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-plot',
   templateUrl: './plot.component.html',
-  styleUrls: ['./plot.component.css'],
+  styleUrls: ['./plot.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PlotComponent implements AfterViewInit {
+export class PlotComponent implements AfterViewInit, OnDestroy {
   @Input() traces: Trace[];
+  onDestroy$ = new Subject();
+  onResize$ = fromEvent(window, 'resize').pipe(
+    debounceTime(100),
+    takeUntil(this.onDestroy$)
+  );
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef<HTMLElement>) { }
 
   ngAfterViewInit() {
     newPlot(this.elementRef.nativeElement, this.traces, {});
+
+    this.onResize$.subscribe(() => {
+      const update = {
+        width: this.elementRef.nativeElement.clientWidth
+      };
+      relayout(this.elementRef.nativeElement, update);
+    });
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
   }
 }
