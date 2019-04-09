66739ee0023ba4760425aa52af4b390b8d448d6f
Felix Lemke <flemke.dev@gmail.com>
2019-04-10 01:44:41 +0200

    refactor(frontend): refactor docs page, add plotly docs link

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './components/app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
+import { DataPersistence } from '@nrwl/nx';
 import { storeFreeze } from 'ngrx-store-freeze';
+
+import { environment } from '../environments/environment';
 import { MatModule } from './mat/mat.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from '@nodeplotlib/pages';
-import { DataPersistence } from '@nrwl/nx';
+
+import { AppComponent } from './components/app/app.component';
 import { components } from './components/index';
 
 @NgModule({
