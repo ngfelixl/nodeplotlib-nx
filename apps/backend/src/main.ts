9d8f95e528ccc01f37568c5636c92892f54dc7b3
Felix Lemke <flemke.dev@gmail.com>
2019-04-10 11:15:53 +0200

    refactor(backend): export server bootstrap function

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
@@ -17,4 +17,3 @@ async function bootstrap() {
   });
 }
 
-bootstrap();
